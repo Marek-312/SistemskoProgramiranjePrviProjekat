eaec9c0f523fc12f22e0dbb8146c101b28249a18
Marko Djordjevic <djordjevic2004marko@gmail.com>
2026-05-10 14:31:51 +0200

    Menjanje GIt ignora

--- obj/Debug/net8.0/SistemskoProgramiranjePrviProjekat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemskoProgramiranjePrviProjekat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemskoProgramiranjePrviProjekat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa98ce914ca8c258f83dbcd8c36923eac7c8809a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3bafaa8246f9c0300527202b822a3867f3e5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemskoProgramiranjePrviProjekat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemskoProgramiranjePrviProjekat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
